3fec2ea2e90d501baf629952653bfb6bb0699ecc
TroyB <troy_budd95@msn.com>
2018-04-20 11:21:24 +0100

    Added the basic functionality of the game, easy difficulty working

--- js/app.js
+++ js/app.js
@@ -1,11 +1,39 @@
+let score = 0;
+let level = 0;
+const scoreCount = document.querySelector('.score');
+const levelCount = document.querySelector('.level')
+let difficulty = '';
+
+const easyBtn = document.querySelector('.easy');
+const medium = document.querySelector('.medium');
+const hard = document.querySelector('.hard');
+
+
+window.onload=function(){
+	alert('Please Select a Difficulty');
+ 	document.querySelector('.easy').addEventListener('click', function(){
+	difficulty = 'easy';
+})
+ 	document.querySelector('.medium').addEventListener('click', function(){
+	difficulty = 'medium';
+})
+ 	document.querySelector('.hard').addEventListener('click', function(){
+	difficulty = 'hard';
+})
+}
+
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+    this.x = x;
+    this.y = y;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +42,31 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+if (difficulty === 'easy') {
+    // Check for enemy going off the screen to reset
+    if (this.x < 505 && level < 5){
+    	this.x += (100 * dt);
+    } else if (this.x < 505 && level < 10){
+    	this.x += (125 * dt); 
+    } else if (this.x < 505 && level < 15){
+    	this.x += (150 * dt); 
+    } else if (this.x < 505 && level < 20){
+    	this.x += (175 * dt); 
+    } else if (this.x < 505 && level < 25){
+    	this.x += (150 * dt); 
+    } else if (this.x < 505 && level > 25){
+    	this.x += (175 * dt); 
+    } else {
+    	this.x = (-90);
+    }
+}
+    
+
+    // Check for enemy / player collision
+    if (this.x < player.x + 45 && this.x + 65 > player.x && this.y < player.y + 60 && this.y + 40 > player.y){
+    	player.reset();
+    };
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +77,61 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+let Player = function(){
+	this.sprite = "images/char-boy.png";
+	this.x = 200;
+	this.y = 300;
 
+}
+
+Player.prototype.update = function() {
+	if(player.y < 20){
+		score++;
+		scoreCount.innerHTML = score;
+		level++;
+		levelCount.innerHTML = level;	
+		this.reset();
+	}
+}
+
+Player.prototype.render = function() {
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(direction) {
+    if(direction == 'left' && this.x > 0) {
+        this.x -= 100;
+    }
+    if(direction == 'right' && this.x < 400) {
+        this.x += 100;
+    }
+    if(direction == 'up' && this.y > 3) {
+        this.y -= 85;
+    }
+    if(direction == 'down' && this.y < 400) {
+        this.y += 85;
+    }
+};
+
+Player.prototype.reset = function() {
+	player.x = 200;
+	player.y = 300;
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+const enemy1 = new Enemy(-100, 60);
+const enemy2 = new Enemy(-210, 140);
+const enemy3 = new Enemy(-375, 230);
+const enemy4 = new Enemy(-400, 60);
+const enemy5 = new Enemy(-550, 140);
+const enemy6 = new Enemy(-770, 230);
 
+// Place all enemy objects in an array called allEnemies
+var allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6];
+// Place the player object in a variable called player
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
